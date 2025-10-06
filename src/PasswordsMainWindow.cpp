5ba783871747cdd64199ca87117d8a048ce4c036
Xavier Callait <xaviercallait@gmail.com>
2025-10-06 10:28:57 -0700

    Created UI for passwords page

--- src/PasswordsMainWindow.cpp
+++ src/PasswordsMainWindow.cpp
@@ -1,21 +1,92 @@
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QDebug>
-#include <stdexcept>
-#include <sodium.h>
+// #include <QLabel>
+// #include <QVBoxLayout>
+// #include <QDebug>
+// #include <stdexcept>
+// #include <sodium.h>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+#include <QKeySequence>
+#include <QToolBar>
+#include <QListWidget>
+#include <QTabWidget>
+#include <QIcon>
+#include <QDockWidget>
+#include <QSplitter>
+#include <QTreeWidget>
+#include <QStatusBar>
+#include <QTableWidget>
 
 #include "PasswordsMainWindow.h"
 
 PasswordsMainWindow::PasswordsMainWindow(std::shared_ptr<Vault> vault, QWidget *parent) : QMainWindow(parent), vault(std::move(vault)) {
 
-    std::vector<unsigned char> nonce = std::vector<unsigned char> (crypto_aead_xchacha20poly1305_ietf_NPUBBYTES);
-    randombytes_buf(nonce.data(), nonce.size());
-    std::string myString = "Hello, World!";
-    std::vector<unsigned char> item(myString.begin(), myString.end());
+    // std::vector<unsigned char> nonce = std::vector<unsigned char> (crypto_aead_xchacha20poly1305_ietf_NPUBBYTES);
+    // randombytes_buf(nonce.data(), nonce.size());
+    // std::string myString = "Hello, World!";
+    // std::vector<unsigned char> item(myString.begin(), myString.end());
 
-    try {
-        qDebug() << this->vault->encryptItem(item, nonce);
-    } catch (std::exception &e) {
-        qDebug() << "RIP";
-    }
+    // try {
+    //     qDebug() << this->vault->encryptItem(item, nonce);
+    // } catch (std::exception &e) {
+    //     qDebug() << "RIP";
+    // }
+
+    QMenuBar *menuBar = new QMenuBar(this);
+    setMenuBar(menuBar);
+
+    QMenu *fileMenu = menuBar->addMenu("File");
+    QAction *newDatabase = new QAction(QIcon(":/icons/Database.svg"), "New Database", this);
+    fileMenu->addAction(newDatabase);
+    QMenu *categoriesMenu = menuBar->addMenu("Categories");
+    QMenu *entryMenu = menuBar->addMenu("Entry");
+
+    QToolBar *toolBar = new QToolBar(this);
+    toolBar->setMovable(false);
+    toolBar->setFloatable(false);
+    toolBar->setContextMenuPolicy(Qt::PreventContextMenu);
+    toolBar->toggleViewAction()->setEnabled(false);
+    addToolBar(Qt::TopToolBarArea, toolBar);
+    toolBar->addAction("Lock");
+    toolBar->addAction("New");
+    toolBar->addAction("Edit");
+    toolBar->addAction("Delete");
+    toolBar->addAction("Share");
+
+    QTabWidget *tabWidget = new QTabWidget(this);
+    setCentralWidget(tabWidget);
+
+    QSplitter *splitter1 = new QSplitter(this);
+    QTreeWidget *groupTree1 = new QTreeWidget(splitter1);
+    groupTree1->setHeaderLabel("Groups");
+    QTableWidget *table1 = new QTableWidget(splitter1);
+    table1->setColumnCount(4);
+    table1->setHorizontalHeaderLabels({"Title", "Password", "URL", "Notes"});
+    splitter1->addWidget(groupTree1);
+    splitter1->addWidget(table1);
+    splitter1->setStretchFactor(1, 2);
+
+    QSplitter *splitter2 = new QSplitter(this);
+    QTreeWidget *groupTree2 = new QTreeWidget(splitter2);
+    groupTree2->setHeaderLabel("Groups");
+    QTableWidget *table2 = new QTableWidget(splitter2);
+    table2->setColumnCount(4);
+    table2->setHorizontalHeaderLabels({"Title", "Password", "URL", "Notes"});
+    splitter2->addWidget(groupTree2);
+    splitter2->addWidget(table2);
+    splitter2->setStretchFactor(1, 2);
+
+    QSplitter *splitter3 = new QSplitter(this);
+    QTreeWidget *groupTree3 = new QTreeWidget(splitter3);
+    groupTree3->setHeaderLabel("Groups");
+    QTableWidget *table3 = new QTableWidget(splitter3);
+    table3->setColumnCount(4);
+    table3->setHorizontalHeaderLabels({"Title", "Password", "URL", "Notes"});
+    splitter3->addWidget(groupTree3);
+    splitter3->addWidget(table3);
+    splitter3->setStretchFactor(1, 2);
+
+    tabWidget->addTab(splitter1, "Database File 1");
+    tabWidget->addTab(splitter2, "Database File 2");
+    tabWidget->addTab(splitter3, "Database File 3");
 }
